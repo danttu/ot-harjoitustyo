57f3e8c81a17a25ee7cefd1d819d30a04e7cd2ec
danttu <32226251+danttu@users.noreply.github.com>
2023-05-13 14:58:04 +0300

    Lisätty docstring

--- harjoitustyo/src/player.py
+++ harjoitustyo/src/player.py
@@ -3,39 +3,75 @@ from turrets import Turret
 
 
 class Player:
+    """Information about player.
+    
+    Args:
+        health: Amount of hitpoints that player's base has.
+        money: Amount of money that player has.
+        current_round: Current round of game.
+        destroyed_enemies: Amount of enemies destroyed.
+        turrets: List of turrets that player owns.
+    """
     def __init__(self):
+        """Player initialization."""
         self.health = 250  
         self.money = 1000  # pylint: disable=invalid-name
-        # Current round limit is 250,
-        # after that enemies have too much speed that they get stuck
         self.current_round = 1
         self.destroyed_enemies = 0
         self.turrets = Turrets()
 
     def add_money(self, amount):
-        # If negative, change to zero
+        """Adds given amount to player's money if valid amount.
+        
+        Args:
+            amount: Amount of money to be given.
+        """
         amount = max(amount, 0)
         self.money += amount
 
     def remove_money(self, amount):
-        # If negative, change to zero
+        """Removes given amount from player's money if valid amount.
+        
+        Args:
+            amount: Amount of money to be taken.
+            
+        Returns:
+            True if amount is not bigger than player's money amount. Else returns false.
+            Used for checking if player was able to purchase turret.
+        """
         amount = max(amount, 0)
-        # If amount is bigger than amount that player has, do not allow to take money
-        # (in this case do not allow player to buy turrets or upgrades)
         if self.money < amount:
             return False
         self.money -= amount
         return True
 
     def buy_turret(self, turret_name, sound_vol, window):
+        """Attempt to buy turret with player's money.
+        
+        Args:
+            turret_name: Name of turret that is being bought.
+            sound_vol: Game's sound volume value.
+            window: Game's window.
+        
+        Returns:
+            True if remove_money() with cost of turret returned true. Else returns false. 
+        """
         turret = Turret(turret_name, sound_vol, window)
-        #if money removal is successful add turret to player owned turrets and return True
         if self.remove_money(turret.cost):
             self.turrets.add_turret(turret)
             return True
         return False
 
     def take_hit(self, hitpoints):
+        """Subtract player health.
+        
+        Args:
+            hitpoints: Amount of hitpoints.
+        
+        Returns:
+            True if hitpoints are bigger that player's current health. Else returns false.
+            Used for game over check.
+        """
         # If negative, change to zero
         hitpoints = max(hitpoints, 0)
         # If damage is bigger than or same as player's health, make player's health to zero
@@ -47,19 +83,25 @@ class Player:
         return False
 
     def get_current_round(self):
+        """Gives game's current round."""
         return self.current_round
     
     def get_money(self):
+        """Gives player's current amount of money."""
         return self.money
     
     def get_health(self):
+        """Gives player's current amount of health"""
         return self.health
     
     def add_destroyed_enemy(self):
+        """Adds one to destroyed enemies counter."""
         self.destroyed_enemies += 1
 
     def get_destroyed_enemies(self):
+        """Gives current amount of destroyed enemies."""
         return self.destroyed_enemies
 
     def next_round(self):
+        """Advances by one round."""
         self.current_round += 1
